b6d221c0c4a20aed2da2771e3e95c9dc5c8ba69e
Suganya S <54302095+suganya-pg@users.noreply.github.com>
2023-08-22 08:33:58 +0000

    completed

---
+++ oddoreven.java
@@ -0,0 +1,10 @@
+class oddoreven {
+    public static void main(String args[])
+    {
+        int num =3;
+        if (num%2==0)
+        System.out.println("The given number is ODD");
+        else
+         System.out.println("The given number is EVEN");
+    }
+}
