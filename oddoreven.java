1adc5a163a882f03c5ac18033421600b847b1014
Suganya S <54302095+suganya-pg@users.noreply.github.com>
2023-08-22 08:39:02 +0000

    completed

--- oddoreven.java
+++ oddoreven.java
@@ -1,7 +1,7 @@
 class oddoreven {
     public static void main(String args[])
     {
-        int num =3;
+        int num=3;
         if (num%2==0)
         System.out.println("The given number is EVEN");
         else
