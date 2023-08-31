72f5b805f89ae668086823aa471dae663badfa13
Suganya S <54302095+suganya-pg@users.noreply.github.com>
2023-08-31 06:58:04 +0000

    c

--- oddoreven.java
+++ oddoreven.java
@@ -5,7 +5,7 @@ class oddoreven {
         Scanner sc=new Scanner(System.in);
         //System.out.println("Enter a number: ");
         //int num=sc.nextInt();
-        int num =10;
+        int num =3;
         if (num%2==0)
         System.out.println("The given number is EVEN");
         else
